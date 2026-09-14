27e718c9245ec58980c2cf30b7ab04a8ac534756
arthexis <arthexis@gmail.com>
2026-09-14 03:28:19 -0600

    Expect Watchtower Arthexis bootstrap role

--- tests/gway/test_rx_ci_contract.py
+++ tests/gway/test_rx_ci_contract.py
@@ -49,7 +49,7 @@ class RxCiContractTests(unittest.TestCase):
         self.assertEqual(
             statements,
             [
-                "install arthexis --service --role [role|Terminal]",
+                "install arthexis --service --role [role|Watchtower]",
                 "arthexis status --json",
                 "arthexis good",
             ],
